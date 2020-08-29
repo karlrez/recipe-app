8227a1875be794204fa88a87c4b425bee9d567c9
karl <karlrez123@gmail.com>
2020-08-29 22:03:06 +0100

    Updates

---
+++ precache-manifest.be3d13f41ae1fd558329a89c57589e8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26a03b846b6f807de60ddaedbc911386",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "b65ebc03acd1c678d6e8",
+    "url": "/recipe-app/static/css/main.391bc977.chunk.css"
+  },
+  {
+    "revision": "7ada5bb8f439859e69ed",
+    "url": "/recipe-app/static/js/2.d7dfb1d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recipe-app/static/js/2.d7dfb1d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b65ebc03acd1c678d6e8",
+    "url": "/recipe-app/static/js/main.6167ce8b.chunk.js"
+  },
+  {
+    "revision": "cd77bb1956273d314d45",
+    "url": "/recipe-app/static/js/runtime-main.14680ac4.js"
+  }
+]);
